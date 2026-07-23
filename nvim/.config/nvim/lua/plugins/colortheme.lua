b7a4474aec99e2fe7ee98509432eeb34e5ed234a
Sn <164057221+nzrrs@users.noreply.github.com>
2026-07-23 15:23:58 +0100

    Add color theme plugins and configure tokyonight
    Added multiple color theme plugins and configured tokyonight.

--- nvim/.config/nvim/lua/plugins/colortheme.lua
+++ nvim/.config/nvim/lua/plugins/colortheme.lua
@@ -1,8 +1,24 @@
+local CURRENT_THEME = "tokyonight" -- Change this
+
 return {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-    transparent = true,
-    }
+	{ "folke/tokyonight.nvim", lazy = false, priority = 1000 },
+	{ "catppuccin/nvim", name = "catppuccin" },
+	{ "rebelot/kanagawa.nvim" },
+	{ "shaunsingh/nord.nvim" },
+	{ "ellisonleao/gruvbox.nvim" },
+	{ "EdenEast/nightfox.nvim" },
+	{ "Mofiqul/vscode.nvim" },
+	{ "projekt0n/github-nvim-theme" },
+
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme(CURRENT_THEME)
+
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		end,
+	},
 }
