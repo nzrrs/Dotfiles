7395ce90c15202216d42c32a413c02cb50de024e
Sn <164057221+nzrrs@users.noreply.github.com>
2026-07-23 15:23:38 +0100

    Update indent-blankline.lua

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,25 +1,11 @@
 return {
-  'lukas-reineke/indent-blankline.nvim',
-  main = 'ibl',
-  opts = {
-    indent = {
-      char = '▏',
-    },
-    scope = {
-      show_start = false,
-      show_end = false,
-      show_exact_scope = false,
-    },
-    exclude = {
-      filetypes = {
-        'help',
-        'startify',
-        'dashboard',
-        'packer',
-        'neogitstatus',
-        'NvimTree',
-        'Trouble',
-      },
-    },
-  },
+	"folke/snacks.nvim",
+	opts = {
+		indent = {
+			enabled = true,
+			animate = {
+				enabled = true,
+			},
+		},
+	},
 }
