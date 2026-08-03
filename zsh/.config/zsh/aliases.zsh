3b9f3d523a33ec3f1ff2b347162d9785e2abcebc
nzrrs <god.nizaryt10@gmail.com>
2026-08-03 01:40:08 +0100

    added c compiler shortcuts

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -13,6 +13,11 @@ alias reboot='sudo reboot'
 alias sleep='systemctl suspend'
 alias logout='gnome-session-quit --logout'
 
+
+#Compiler 
+alias gccW='gcc -Wall -Wextra -Wall'
+alias gccS='gcc -Wall -Wextra -Wall -fsanitize=address'
+
 # git
 alias gs='git status'
 alias ga='git add'
